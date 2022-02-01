408abef3380b94a7dea6d7a547985c55ae1d1ec8
almostbergman <almost.bergman@gmail.com>
2022-02-01 20:48:27 +0100

    206._reverse_linked_list: js + rust solutions

---
+++ linked_list/206._reverse_linked_list/index.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+  if (!head || !head.next) return head;
+
+  const reversed = reverseList(head.next);
+
+  head.next.next = head;
+  head.next = null;
+
+  return reversed;
+};
