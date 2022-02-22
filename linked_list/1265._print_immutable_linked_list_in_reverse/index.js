563664ea3b9c5505ac203d4483797e5d6b71d4be
almostbergman <almost.bergman@gmail.com>
2022-02-22 23:17:52 +0100

    1265: add js solution

---
+++ linked_list/1265._print_immutable_linked_list_in_reverse/index.js
@@ -0,0 +1,29 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    };
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+const printLinkedListInReverse = (head) => {
+  if (!head) return null;
+
+  if (head.getNext()) {
+    printLinkedListInReverse(head.getNext());
+  }
+
+  return head.printValue();
+};
